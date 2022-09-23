c09ad8b6521103bad65ebacbd17d869e164394c2
Adame Barhdadi <adame.barhdadi@student.vinci.be>
2022-09-23 10:30:43 +0200

    begin 2.2

--- module2/2.1/src/index.js
+++ module2/2.1/src/index.js
@@ -1,2 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+
+
+
+/*
+import centerImg from './img/centerIcon.png';
+
+
+const homePage = `
+<img src="${centerImg}" alt="centerIcon" > <h1>My Movies</h1> 
+
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+const main = document.querySelector('main');
+
+main.innerHTML = homePage;
+*/
