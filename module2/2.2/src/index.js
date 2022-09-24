5e423802f3a66664f033589cf61b6ca26c3393f2
adam <adame.barhdadi@student.vinci.be>
2022-09-24 14:41:59 +0200

    myMovies : module 2 : dynamic homepage

---
+++ module2/2.2/src/index.js
@@ -0,0 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+
+import centerIcon from './img/centerIcon.png'
+import avg1 from './img/avg1.png';
+import avg2 from './img/avg2.png';
+import avg3 from './img/avg3.png';
+
+
+
+const homepage = `
+<img src="${centerIcon}" alt="centerIcon" > <h1>My Movies</h1> 
+
+<div class="liste">
+  <div id="AlreadySeen">
+  <ul><h4>Already Seen movies</h4>
+    <li><img src="${avg1}" alt="avengers1"></li>
+    <li><img src="${avg2}" alt="avengers2"></li>
+    <li><img src="${avg3}" alt="avengers3"></li>
+</div>
+</ul>
+<div id="ToSee">
+<ul><h4>List:</h4>
+<li><img src="${avg1}" alt="avengers1"></li>
+    <li><img src="${avg2}" alt="avengers2"></li>
+    <li><img src="${avg3}" alt="avengers3"></li>
+</div>
+</ul>
+<div id="Reccomandation">
+<ul><h4>Recommandation:</h4>
+<li><img src="${avg1}" alt="avengers1"></li>
+    <li><img src="${avg2}" alt="avengers2"></li>
+    <li><img src="${avg3}" alt="avengers3"></li>
+</div>
+</ul>
+
+</div>`
+
+
+const main = document.querySelector('main');
+main.innerHTML = homepage;
