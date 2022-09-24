390805ab69cc4a442917a91df4af815359b4a05b
adam <adame.barhdadi@student.vinci.be>
2022-09-24 14:30:49 +0200

    myMovies : bundler

--- module2/2.1/src/index.js
+++ module2/2.1/src/index.js
@@ -4,28 +4,3 @@ import './stylesheets/main.css';
 
 
 
-/*
-import centerImg from './img/centerIcon.png';
-
-
-const homePage = `
-<img src="${centerImg}" alt="centerIcon" > <h1>My Movies</h1> 
-
-`;
-
-
-
-
-
-
-
-
-
-
-
-
-
-const main = document.querySelector('main');
-
-main.innerHTML = homePage;
-*/
